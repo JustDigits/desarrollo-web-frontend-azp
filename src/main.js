fb74206b9330f1b8cfd0341386a776bb9fa04d5a
JustDigits <112163363+JustDigits@users.noreply.github.com>
2024-09-01 03:23:21 -0600

    Variables y Constantes

---
+++ src/main.js
@@ -0,0 +1,13 @@
+let name = 'Alex';
+name = 'Alejandro';
+
+const lastname = 'Rodríguez';
+// lastname = 'Martínez';
+
+const condition = true;
+if (condition) {
+  const lastname = 'Hyper';
+  console.log(lastname);
+}
+
+console.log('Hola Mundo!!! ' + name + ' ' + lastname);
