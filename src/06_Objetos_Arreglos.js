e92acb32505009e59ff181574793f16cc0da7c92
JustDigits <112163363+JustDigits@users.noreply.github.com>
2024-10-01 23:05:54 -0600

    Operador Spread

--- src/06_Objetos_Arreglos.js
+++ src/06_Objetos_Arreglos.js
@@ -43,16 +43,4 @@ const carro = {
 };
 
 console.log(carro);
-
-// Misma referencia
-// const carro2 = carro;
-
-// Operador spread
-const carro2 = { ...carro };
-const valida = carro2 === carro;
-
-console.log(valida);
-
-carro2.id = 20;
-console.log(carro.id);
-console.log(carro2.id);
+console.log(carro.totalInventario());
